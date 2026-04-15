b037c4f4d2883fd086648bfbe7dc1e68e70ec40c
sgcornej <sgcornej@ncsu.edu>
2026-04-15 09:57:24 -0400

    black formatting

--- activities/SaraCornejo_waterflow.py
+++ activities/SaraCornejo_waterflow.py
@@ -8,21 +8,39 @@ import grass.script as gs
 
 def run_waterflow(scanned_elev, env, **kwargs):
     # first we need to compute x- and y-derivatives
-    gs.run_command('r.slope.aspect', elevation=scanned_elev, dx='scan_dx', dy='scan_dy', env=env)
-    gs.run_command('r.sim.water', elevation=scanned_elev, dx='scan_dx', dy='scan_dy',
-                   rain_value=150, depth='flow', env=env)
-    
+    gs.run_command(
+        "r.slope.aspect", elevation=scanned_elev, dx="scan_dx", dy="scan_dy", env=env
+    )
+    gs.run_command(
+        "r.sim.water",
+        elevation=scanned_elev,
+        dx="scan_dx",
+        dy="scan_dy",
+        rain_value=150,
+        depth="flow",
+        env=env,
+    )
+
+
 def run_wateraccum(scanned_elev, env, **kwargs):
     repeat = 2
     input_dem = scanned_elev
     output = "tmp_filldir"
     for i in range(repeat):
-        gs.run_command('r.fill.dir', input=input_dem, output=output, direction="tmp_dir", env=env)
+        gs.run_command(
+            "r.fill.dir", input=input_dem, output=output, direction="tmp_dir", env=env
+        )
         input_dem = output
     # filter depressions deeper than 0.1 m
-    gs.mapcalc('{new} = if({out} - {scan} > 0.1, {out} - {scan}, null())'.format(new='ponds', out=output,
-                                                                                  scan=scanned_elev), env=env)
-    gs.write_command('r.colors', map='ponds', rules='-', stdin='0% aqua\n100% blue', env=env)
+    gs.mapcalc(
+        "{new} = if({out} - {scan} > 0.1, {out} - {scan}, null())".format(
+            new="ponds", out=output, scan=scanned_elev
+        ),
+        env=env,
+    )
+    gs.write_command(
+        "r.colors", map="ponds", rules="-", stdin="0% aqua\n100% blue", env=env
+    )
 
 
 def main():
@@ -34,6 +52,7 @@ def main():
     gs.run_command("r.resamp.stats", input=elevation, output=elev_resampled, env=env)
     run_waterflow(scanned_elev=elev_resampled, env=env)
     run_wateraccum(scanned_elev=elev_resampled, env=env)
+
 
 if __name__ == "__main__":
     main()
